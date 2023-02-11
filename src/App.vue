a59103a7ed6937dfee36f3c13a9078d52705257a
exe <igorwdowiczak@gmail.com>
2023-02-11 16:41:57 +0100

    :anchor: Added fonts and created Board component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Board />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: Config;
+  src: local("Config"),
+    url(./fonts/Config/ConfigCondensed-MediumItalic.otf) format("truetype");
 }
 </style>
