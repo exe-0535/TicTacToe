85981b2c565442355d4ff6ffade493273ee6fb11
exe <igorwdowiczak@gmail.com>
2023-02-16 21:21:36 +0100

    :anchor: Added border

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,20 @@ const winner = computed(() => CalculateWinner(board.value.flat()));
 
 <template>
   <main class="pt-8 text-center bg-slate-800 min-h-screen">
+
     <h1 class="mb-8 text-8xl font-bold uppercase ">Tic Tac Toe</h1>
-    <h3 class="text-6xl mb-4">It's now {{ player }}'s turn</h3>
-  </main>
+
+    <h3 class="text-6xl mb-20">It's now {{ player }}'s turn</h3>
+
+    <div class="flex flex-col items-center mb-9">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <div v-for="(cell, y) in row" :key="y" @click="MakeMove(x, y)"
+          :class="`border-2 border-white w-40 h-40 hover:bg-slate-700 duration-500 flex items-center justify-center text-4xl cursor-pointer`">
+        </div>
+      </div>
+    </div>
+
+</main>
 </template>
 
 
